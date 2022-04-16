f4d3a510e4ef86b7413b048908e5010c2e5669b6
Mariia Kucheieva <mary.kucheeva@gmail.com>
2022-04-16 19:21:14 +0200

    cursor

--- app/js/phonemask.js
+++ app/js/phonemask.js
@@ -4,6 +4,6 @@
 
 const element = document.querySelector('.phone__input')
 const maskOptions = {
-  mask: '+{3}(000)000-00-00'
+  mask: '+{43}(000)000-000-00'
 }
 const mask = IMask(element, maskOptions)
