aa579813c2e1054bdf4a0cb9a403e92173e6e370
Mariia Kucheieva <mary.kucheeva@gmail.com>
2022-04-16 17:11:44 +0200

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,21 @@
 // Smooth scroll https://github.com/cferdinandi/smooth-scroll
-// new SmoothScroll('a[href*="#"]')
+new SmoothScroll('a[href*="#"]')
+
+
+/// Slider
+new Swiper('.swiper-container', {
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: '.arrow__btn-next',
+    prevEl: '.arrow__btn-prev',
+  },
+  breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      }
+    }
+});
